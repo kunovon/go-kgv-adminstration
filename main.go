3286783151d62e1e6440aca1992795be57dbcb4f
Markus <mailformarkus@gmx.de>
2023-11-22 10:47:45 +0100

    webserver with / and /about

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// main Function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		}
-		fmt.Println(fmt.Sprintf("Number of ytes written: %d", n))
-	})
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
 
-	_ = http.ListenAndServe(":8080", nil)
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("<h1>This is the about page and 2 + 2 is %d.</h1>", sum))
+}
+
+// addValues adds two ints x and y, and returns the sum
+func addValues(x, y int) int {
+	return x + y
 }
