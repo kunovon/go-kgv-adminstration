670fdd7a8e9300c92d973ab4f9338241f37e6bef
markus <mailformarkus@gmx.de>
2023-11-23 18:57:45 +0100

    ohne Templates

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -10,20 +9,18 @@ const portNumber = ":8080"
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	fmt.Fprintf(w, "This is the home page")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d"), sum)
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
+// addValues adds two ints x and y, and returns the sum
+func addValues(x, y int) int {
+	return x + y
 }
 
 // main is the main function
