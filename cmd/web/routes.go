b580129bebb4df411d94785bf3c216f7b1338e1c
Markus <mailformarkus@gmx.de>
2024-01-29 19:34:39 +0100

    fileserver and example picture

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -29,5 +29,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/impressum", handlers.Repo.Impressum)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
